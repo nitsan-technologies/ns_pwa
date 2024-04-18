67dcf440e6ff2371cc72b85c24ec66a68dbe566c
NITSAN <info@nitsan.in>
2024-04-18 19:01:45 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,8 +3,7 @@
 $EM_CONF['ns_pwa'] = [
     'title' => 'TYPO3 PWA (Progressive Web Apps) Extension',
     'description' => 'Make your TYPO3 website lightning fast & work offline! Get our TYPO3 PWA extension for a better SEO & mobile experience. Install today!
-*** Live Demo: https://demo.t3planet.com/t3-extensions/typo3-pwa *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-pwa-extension
-',
+    *** Live Demo: https://demo.t3planet.com/t3-extensions/typo3-pwa *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-pwa-extension',
     'category' => 'plugin',
     'author' => 'T3: Jay Bhalgamiya, T3: Arun Solanki, T3: Nilesh Malankiya, QA: Krishna Dhapa',
     'author_email' => 'sanjay@nitsan.in',
